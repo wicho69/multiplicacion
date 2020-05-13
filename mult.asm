e2b8a995076ae422977d297d48aeb650531f6977
wicho69 <61756141+wicho69@users.noreply.github.com>
2020-05-13 17:35:46 -0500

    Update mult.asm

--- mult.asm
+++ mult.asm
@@ -6,22 +6,22 @@
     GOTO CONF
 
 CONF
-    VAR1 EQU 0x30  ;Nombro el registro  como VAR1
-    VAR2 EQU 0x31  ;Nombro el registro  como VAR2
-    MOVLW d'5'   ;Escribo el numero 3 decimal en el registro W
-    MOVWF VAR1	    ;Muevo el valor de W a VAR1	
-    MOVLW d'3' ;Escribo el numero 2 decimal en el registro W
-    MOVWF VAR2	    ;Muevo el valor de W a VAR2	
+    VAR1 EQU 0x30  
+    VAR2 EQU 0x31  
+    MOVLW d'5'   
+    MOVWF VAR1	    	
+    MOVLW d'3' 
+    MOVWF VAR2	    
     nop
-    GOTO OPER	    ;Ir hasta la etiqueta OPER
+    GOTO OPER	   
     
 SUM 
-    ADDWF VAR2,0    ;Sumar el valor de VAR2 al registro W
-    GOTO OPER	    ;Ir hasta la etiqueta OPER
+    ADDWF VAR2,0    
+    GOTO OPER	    
   
 OPER
-    DECFSZ VAR1,1   ;Decrementar el valor de VAR1 y reasignarlo al mismo registro, 
-    GOTO SUM	    ;Ir hasta la etiqueta SUM
-    MOVWF VAR2	    ;Mover el valor de W a VAR2
+    DECFSZ VAR1,1   
+    GOTO SUM	   
+    MOVWF VAR2	    
     nop
     END
